8ab823c6200c50cbfb73897124f884d9c29623f2
AleksandraSzendzielorz <kontaktowyoficjalny@gmail.com>
2024-03-30 16:44:57 +0100

    Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import { Profile } from './components/Profile';
+import { Statistics } from './components/Statistics';
+import { FriendList } from './components/FriendList';
+import { TransactionHistory } from './components/TransactionHistory';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +12,13 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile />
+      <Statistics />
+      <FriendList />
+      <TransactionHistory />
     </div>
   );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import user from './src/user.json/user/json';
+
+export const Profile = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+};
