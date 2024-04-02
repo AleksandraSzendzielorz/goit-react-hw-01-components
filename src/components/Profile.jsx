655ce8e9f1bc49436ec29e69472643c011ebf4fd
AleksandraSzendzielorz <kontaktowyoficjalny@gmail.com>
2024-04-02 10:52:39 +0200

    commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -70,7 +70,6 @@ export const Profile = () => {
           </li>
         </ul>
       </div>
-      ;
     </>
   );
 };
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import data from 'jsonfolder/data.json';
 
+const statisticList = {
+  display: 'flex',
+  listStyle: 'none',
+  margin: 0,
+  padding: 0,
+};
+
+const statisticListLi = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: 150,
+  textAlign: 'center',
+};
+
 export const Statistics = ({ title }) => {
   const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
@@ -14,12 +28,12 @@ export const Statistics = ({ title }) => {
   return (
     <section className="statistics">
       {title && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">
+      <ul style={statisticList} className="stat-list">
         {data.map((stat, index) => (
           <li
             key={index}
             className="item"
-            style={{ backgroundColor: getRandomColor() }}
+            style={{ backgroundColor: getRandomColor(), ...statisticListLi }}
           >
             <span className="label">{stat.label}</span>
             <span className="percentage">{stat.percentage}%</span>
